fc9769d24c8173358e14fafd35585930cb266158
Anita Stasiełuk <a.stasieluk99@gmail.com>
2022-12-04 17:47:03 +0100

    image change

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,9 +1,9 @@
 import './components.css';
-import marty from '../images/marty.png'
+import janek from '../images/janek.png'
 function Canvas() {
     return (
     <div className={'left-div'}>
-        <img id="image" src={marty} style={{height: "80px",  display: "block", margin: "30% auto auto"}} alt="robot marty" />
+        <img id="image" src={janek} style={{height: "150px",  display: "block", margin: "30% auto auto"}} alt="robot janek" />
     </div>
     );
 }
